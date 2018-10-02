528b120052fbdc08f784a7e64741496dd35a10c4
steven243 <43686665+steven243@users.noreply.github.com>
2018-10-02 09:58:08 -0500

    Add files via upload

---
+++ prueba.py
@@ -0,0 +1,14 @@
+import pusher
+
+pusher_client = pusher.Pusher(
+  app_id='611990',
+  key='22f110291d714ce8eaaa',
+  secret='f4ab8cbd732e3e6b49d7',
+  cluster='mt1',
+  ssl=True
+)
+
+i = 0
+while True:
+    i+= 1
+    pusher_client.trigger('my-channel', 'my-event', {'sl': i, 'sw': i, 'pl': i, 'pw': i})
