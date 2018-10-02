b872ea51300141422f258d32872c465d05506735
steven243 <43686665+steven243@users.noreply.github.com>
2018-10-02 11:20:12 -0500

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,6 +1,6 @@
 import pusher
 
-pusher_client = pusher.Pusher(
+client = pusher.Pusher(
   app_id='611990',
   key='22f110291d714ce8eaaa',
   secret='f4ab8cbd732e3e6b49d7',
@@ -11,4 +11,4 @@ pusher_client = pusher.Pusher(
 i = 0
 while True:
     i+= 1
-    pusher_client.trigger('my-channel', 'my-event', {'sl': i, 'sw': i, 'pl': i, 'pw': i})
+    client.trigger('my-channel', 'my-event', {'sl': i, 'sw': i, 'pl': i, 'pw': i})
