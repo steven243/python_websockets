9236834ca44f68ecd97c2db1a6b27bda38ee2a58
steven243 <43686665+steven243@users.noreply.github.com>
2018-10-02 11:13:54 -0500

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,10 +1,10 @@
 import pusher
 
 pusher_client = pusher.Pusher(
-  app_id='611990',
-  key='22f110291d714ce8eaaa',
-  secret='f4ab8cbd732e3e6b49d7',
-  cluster='mt1',
+  app_id="611990",
+  key="22f110291d714ce8eaaa",
+  secret="f4ab8cbd732e3e6b49d7",
+  cluster="mt1",
   ssl=True
 )
 
